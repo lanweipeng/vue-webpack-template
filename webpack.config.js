7a1960d7d64ee30c0d63a948327141627cf87ac6
lanweipeng <1462191066@qq.com>
2020-04-18 14:43:12 +0800

    ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path=require('path')
 const VueLoaderPlugin=require('vue-loader/lib/plugin')
 module.exports={
   // model:'none',
-  entry:'./src/main.js',
+  entry:'./src/main.ts',
   output:{
     path:path.resolve(__dirname,'dist'),
     filename:'main.min.js'
@@ -10,7 +10,15 @@ module.exports={
   module:{
     rules:[
       {test:/\.css$/,use:['style-loader','css-loader']},
-      {test:/\.vue$/,use:['vue-loader']}
+      {test:/\.vue$/,use:['vue-loader']},
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+            appendTsSuffixTo: [/\.vue$/]
+        }
+}
     ]
   },
   resolve:{
