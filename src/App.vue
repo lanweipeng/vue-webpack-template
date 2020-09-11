f6361d4f2958187668e92720f90af0aed5aa2e61
兰为鹏 <1462191066@qq.com>
2020-09-11 11:21:55 +0800

    最简单的vue-webpack-template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
-// import Vue from 'vue'
-// export default Vue.component('App',{
-// data(){
-// return {
-//   xx:'wnima'
-// }
-// },
-// methods:{
-//   ff(){
-//     let nine='99'
-//     this.xx=nine
-//   }
-// },
-// template:`
-// <div @click="ff()">hh{{xx}}</div>
-// `,
-// created(){},components:{}
-// })
 <template>
-<div>gg{{}}</div>
+<div>hello</div>
 </template>
 <script>
 export default {
